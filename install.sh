e0215639139906c60fe2a03529263466460adaf0
timmPrice <timmPrice6@gmail.com>
2024-05-09 16:32:59 -0400

    adding .bashrc for PS1 variable

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/bsh
 
+sudo add-apt-repository ppa:maveonair/helix-editor
+sudo apt update
+sudo apt install helix
+
 ln -sf ~/.dotfiles/nvim ~/.config/nvim
 ln -sf ~/.dotfiles/alacritty ~/.config/alacritty
 ln ~/.dotfiles/.tmux.conf ~/
 ln -sf ~/.dotfiles/helix ~/.config/helix
+ln ~/.dotfiles/.bashrc ~/
 
 sudo apt install sl
 
