449de65bfb44e723574cd0007698157bdc873449
timmPrice <timmprice6@gmail.com>
2024-05-09 16:22:20 -0400

    adding helix editor configs

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 ln -sf ~/.dotfiles/nvim ~/.config/nvim
 ln -sf ~/.dotfiles/alacritty ~/.config/alacritty
 ln ~/.dotfiles/.tmux.conf ~/
+ln -sf ~/.dotfiles/helix ~/.config/helix
 
 sudo apt install sl
 
