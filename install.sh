0f20e7db25d95fc765208b0f66b7f3ab1eadec0c
timmPrice <timmprice6@gmail.com>
2024-07-14 18:31:08 -0400

    fixing install.sh to work within repos directory and fixing helix installation

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
 #!/bin/bsh
 
+# adding and installing helix editor
 sudo add-apt-repository ppa:maveonair/helix-editor
 sudo apt update
 sudo apt install helix
+rm -r ~/.config/helix
 
-ln -sf ~/.dotfiles/nvim ~/.config/nvim
-ln -sf ~/.dotfiles/alacritty ~/.config/alacritty
-ln ~/.dotfiles/.tmux.conf ~/
-ln -sf ~/.dotfiles/helix ~/.config/helix
-ln ~/.dotfiles/.bashrc ~/
-cp ~/.dotfiles/.bash-completion ~/
+# symlinking .config files
+ln -sf ~/repos/.dotfiles/nvim ~/.config/nvim
+ln -sf ~/repos/.dotfiles/alacritty ~/.config/alacritty
+ln ~/repos/.dotfiles/.tmux.conf ~/
+ln -sf ~/repos/.dotfiles/helix ~/.config/helix
+ln ~/repos/.dotfiles/.bashrc ~/
+cp ~/repos/.dotfiles/.bash-completion ~/
 
+#goofy ahh program
 sudo apt install sl
 
-echo "👺  !symlinked .dotfiles! 👺"
+echo "🍜 !symlinked .dotfiles! 🍜"
