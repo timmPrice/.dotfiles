7237da1086f0249380a194974343c99e3ef9bf30
timmPrice <timmPrice6@gmail.com>
2024-11-14 22:55:14 -0500

    updating scripts

--- scripts/programs.sh
+++ scripts/programs.sh
@@ -11,16 +11,23 @@
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt update && upgrade
 
-sudo apt install wget
-sudo apt install curl
-sudo apt install snapd
-sudo apt install nvim
-sudo apt install tmux
-sudo apt install kitty
-sudo apt install zsh
-sudo apt install gnome-tweaks
-sudo apt install git
-sudo apt install code
+apt_programs=(
+    wget
+    curl
+    snapd
+    nvim
+    tmux
+    kitty
+    zsh
+    gnome-tweaks
+    git
+    code
+    python3
+    pip
+    gcc
+)
+
+sudo apt install "${apt_programs[@]}"
 
 #----------------------------------------#
 # Installing applications via Snap Store #
@@ -33,13 +40,6 @@ sudo snap install discord
 #--------------------------#
 # Programming dependencies # 
 #--------------------------#
-
-# python
-sudo apt install python3
-sudo apt install pip
-
-# C
-sudo apt install gcc
 
 # Ocaml
 bash -c "sh <(curl -fsSL https://opam.ocaml.org/install.sh)"
