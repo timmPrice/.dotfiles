b0cd5a118fee2b7d7232e46f0612eaf5d1cd587e
timmPrice <timmPrice6@gmail.com>
2025-01-04 10:32:13 -0500

    stuff

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ all: run-scripts
 
 run-scripts:
 	python3 python-scripts/install.py	
-	sudo bash scripts/fonts.sh
+	# sudo bash scripts/fonts.sh
 	sudo bash scripts/terminal.sh
 	sudo bash scripts/ssh.sh
 	sudo bash scripts/symlink.sh
--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
 ln -sf ~/repos/.dotfiles/nvim ~/.config/nvim
-# sync packer and update shout out plugin files!
-
-ln -sf ~/repos/.dotfiles/zsh/.zshrc ~/
 ln -sf ~/repos/.dotfiles/kitty ~/.config/kitty
 ln ~/repos/.dotfiles/.tmux.conf ~/
 ln ~/repos/.dotfiles/.gitconfig ~/
-ln -sf ~/repos/.dotfiles/tms/ ~/.config
-ln -sf ~repos/.dotfiles/fish ~/.config
-
-# i3 window manager
-# ln -sf ~/repos/.dotfiles/i3 ~/.config/i3
-
-# Hyprland window manager
-# ln -sf ~/repos/.dotfiles/hypr ~/.config/hypr
+ln -sf ~repos/.dotfiles/fish ~/.config/fish
