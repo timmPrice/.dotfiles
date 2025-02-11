981b6e92f52ebb04f0fe57bea644d013a2658c0c
timmPrice <timmPrice6@gmail.com>
2025-02-11 00:22:34 -0500

    adding sessionizer command keybind

--- env/.config/nvim/lua/config/remap.lua
+++ env/.config/nvim/lua/config/remap.lua
@@ -30,6 +30,10 @@ vim.keymap.set("n", "<leader>j", "<C-w>j", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>k", "<C-w>k", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>l", "<C-w>l", { noremap = true, silent = true })
 
+-- tmux sessionizer
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+-- find and replace
 vim.keymap.set("n", "<leader>s", function()
     local find = vim.fn.input("Find: ")
     local replace = vim.fn.input("Replace with: ")
