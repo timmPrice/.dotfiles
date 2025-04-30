d72bcc704c6c9e680d5e23a3b8b8a24cd1e6aa9c
timmPrice <timmPrice6@gmail.com>
2025-04-30 10:43:00 -0400

    pushing whatever changed in the last ~ 2 weeks

--- env/.config/nvim/lua/config/plugins/colors.lua
+++ env/.config/nvim/lua/config/plugins/colors.lua
@@ -1,11 +1,22 @@
 return {
     -- "timmPrice/sitruuna.vim",
     -- "aditya-azad/candle-grey",
-    "ellisonleao/gruvbox.nvim",
+    -- "ellisonleao/gruvbox.nvim",
+    -- config = function()
+    --     require("gruvbox").setup({
+    --         transparent_mode = true, 
+    --     }) 
+    --     vim.cmd.colorscheme "gruvbox"
+    -- end
+    
+    "ramojus/mellifluous.nvim",
     config = function()
-        require("gruvbox").setup({
-            transparent_mode = true, 
-        }) 
-        vim.cmd.colorscheme "gruvbox"
-    end
+        require("mellifluous").setup({
+            styles = {
+                main_keywords = {bold = true} 
+            },
+        })
+        vim.opt.background = "light"
+        vim.cmd.colorscheme "mellifluous"
+    end,
 }
