4c2940fc2cfe723051b146cdeb2de785c0b4b79f
timmPrice <timmPrice6@gmail.com>
2025-07-23 13:24:29 -0400

    making my colors ugly

--- env/.config/nvim/lua/config/plugins/colors.lua
+++ env/.config/nvim/lua/config/plugins/colors.lua
@@ -1,11 +1,22 @@
 return {
     -- "timmPrice/sitruuna.vim",
     -- "aditya-azad/candle-grey",
-    "ellisonleao/gruvbox.nvim",
+    -- "ellisonleao/gruvbox.nvim",
+    -- config = function()
+    --     require("gruvbox").setup({
+    --         transparent_mode = true, 
+    --     }) 
+    --     vim.cmd.colorscheme "gruvbox"
+    -- end
+    "rose-pine/neovim",
+    name = "rose-pine",
     config = function()
-        require("gruvbox").setup({
-            transparent_mode = true, 
-        }) 
-        vim.cmd.colorscheme "gruvbox"
+        require("rose-pine").setup({
+            variant = "moon",
+            styles = {
+                transparency = true,
+            },
+        })
+        vim.cmd("colorscheme rose-pine")
     end
 }
