8c593f96585b0ac5bcf4c519ec21aabd7cecf807
timmPrice <timmPrice6@gmail.com>
2025-07-22 22:22:04 -0400

    abacadaba

--- env/.config/nvim/lua/config/plugins/colors.lua
+++ env/.config/nvim/lua/config/plugins/colors.lua
@@ -1,17 +1,11 @@
 return {
     -- "timmPrice/sitruuna.vim",
     -- "aditya-azad/candle-grey",
-    -- "ellisonleao/gruvbox.nvim",
-    -- config = function()
-    --     require("gruvbox").setup({
-    --         transparent_mode = true, 
-    --     }) 
-    --     vim.cmd.colorscheme "gruvbox"
-    -- end
-      "forest-nvim/sequoia.nvim",
-      lazy = false,
-      priority = 1000,
-      config = function()
-        vim.cmd("colorscheme sequoia") -- or 'sequoia-night' / 'sequoia-rise'
-      end,
+    "ellisonleao/gruvbox.nvim",
+    config = function()
+        require("gruvbox").setup({
+            transparent_mode = true, 
+        }) 
+        vim.cmd.colorscheme "gruvbox"
+    end
 }
