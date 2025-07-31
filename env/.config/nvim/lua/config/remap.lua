2752c5d3d937fdaefa95d7383d5ef1f32220f1c8
timmPrice <timmPrice6@gmail.com>
2025-07-31 11:37:19 -0400

    making new really nice keybindings for nvim -> visual block remaped and indenting remains highlighted

--- env/.config/nvim/lua/config/remap.lua
+++ env/.config/nvim/lua/config/remap.lua
@@ -47,6 +47,10 @@ vim.keymap.set("n", "<leader>s", function()
     vim.cmd(string.format("%%s/%s/%s/g", vim.fn.escape(find, "/"), vim.fn.escape(replace, "/")))
 end)
 
+vim.keymap.set("n", "<C-s>", "<C-v>")
+vim.keymap.set("x", "<", "<gv")
+vim.keymap.set("x", ">", ">gv")
+
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader><leader>", function()
