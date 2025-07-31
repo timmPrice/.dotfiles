2a2fdc6cb54725251dae9b26434e03cd6b71cd26
timmPrice <timmPrice6@gmail.com>
2025-07-30 22:59:38 -0400

    actually settling on a good nvim colorscheme

--- env/.config/nvim/lua/config/plugins/colors.lua
+++ env/.config/nvim/lua/config/plugins/colors.lua
@@ -1,13 +1,16 @@
 return {
     -- "timmPrice/sitruuna.vim",
     -- "aditya-azad/candle-grey",
-    "ellisonleao/gruvbox.nvim",
-    config = function()
-        require("gruvbox").setup({
-            transparent_mode = true, 
-        }) 
-        vim.cmd.colorscheme "gruvbox"
-    end
+    -- config = function()
+    --     vim.cmd.colorscheme "candle-grey" 
+    -- end
+    -- "ellisonleao/gruvbox.nvim",
+    -- config = function()
+    --     require("gruvbox").setup({
+    --         transparent_mode = true, 
+    --     }) 
+    --     vim.cmd.colorscheme "gruvbox"
+    -- end
     -- "rose-pine/neovim",
     -- name = "rose-pine",
     -- config = function()
@@ -19,4 +22,37 @@ return {
     --     })
     --     vim.cmd("colorscheme rose-pine")
     -- end
+    "vague2k/vague.nvim",
+    config = function()
+        require("vague").setup({
+            transparent = true,
+            bold = true,
+            italic = true,
+            colors = {
+                bg = "#141415",
+                fg = "#cdcdcd",
+                floatBorder = "#878787",
+                line = "#252530",
+                comment = "#606079",
+                builtin = "#b4d4cf",
+                func = "#c48282",
+                string = "#e8b589",
+                number = "#e0a363",
+                property = "#c3c3d5",
+                constant = "#aeaed1",
+                parameter = "#bb9dbd",
+                visual = "#333738",
+                error = "#d8647e",
+                warning = "#f3be7c",
+                hint = "#7e98e8",
+                operator = "#90a0b5",
+                keyword = "#6e94b2",
+                type = "#9bb4bc",
+                search = "#405065",
+                plus = "#7fa563",
+                delta = "#f3be7c",
+            },
+        })
+        vim.cmd("colorscheme vague")
+    end
 }
