0b05a111c42ce8a3772e95f4bec3fe93b4f2a358
timmPrice <timmPrice6@gmail.com>
2025-08-22 15:47:31 -0400

    fixing bun path

--- env/.config/fish/config.fish
+++ env/.config/fish/config.fish
@@ -10,6 +10,8 @@ if test -d "$HOME/.local/bin/elixir/bin"
     set -gx PATH $HOME/.local/bin/elixir/bin $PATH
 end
 
+set -gx PATH $HOME/timprice/.bun/bin/bun $PATH
+
 set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
 
 set -U fish_user_paths ~/.local/bin $fish_user_paths
