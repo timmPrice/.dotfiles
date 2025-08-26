8c854fd321d7a50c2fe65dc52a58abf69cfa99b0
timmPrice <timmPrice6@gmail.com>
2025-08-26 12:50:04 -0400

    fixing bun path

--- env/.config/fish/conf.d/config_paths.fish
+++ env/.config/fish/conf.d/config_paths.fish
@@ -3,4 +3,4 @@ set -gx PATH "$HOME/.config/emacs/bin" $PATH
 set -gx PATH "/usr/local/go/bin/" $PATH
 set -gx PATH "$HOME/go/bin/" $PATH
 set -Ux PYTHONUSERBASE $HOME/.local 
-set -Ux PATH $HOME/.bun/bin $PATH
+set -Ux fish_user_paths $HOME/.bun/bin/ $PATH
--- env/.config/fish/config.fish
+++ env/.config/fish/config.fish
@@ -10,8 +10,6 @@ if test -d "$HOME/.local/bin/elixir/bin"
     set -gx PATH $HOME/.local/bin/elixir/bin $PATH
 end
 
-set -gx PATH $HOME/timprice/.bun/bin/bun $PATH
-
 set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
 
 set -U fish_user_paths ~/.local/bin $fish_user_paths
